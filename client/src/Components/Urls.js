af819aadb441cce0f5336e27f689f11a671e228a
Ravi Patidar <141826912+Ravi16Patidar@users.noreply.github.com>
2024-09-16 13:37:10 +0530

    Update Urls.js

--- client/src/Components/Urls.js
+++ client/src/Components/Urls.js
@@ -1,2 +1,4 @@
 // export const baseUrl='https://blogify-77h8.onrender.com'
-export const baseUrl='http://localhost:5000'
+// export const baseUrl='http://localhost:5000'
+export const baseUrl="https://blogify-server-ten.vercel.app"
+
